acafb0554aa1d2deb05ebb48be8df9a54cf11148
Tomoyo Alvåg <tomoyo.tomoyo.m@gmail.com>
2024-03-07 15:25:09 +0100

    add products on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
-import { Box } from "@chakra-ui/react";
+import { products } from "@/data";
+import { Box, Grid, GridItem, Image } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Box>
       <p>Det här är startsidan. Här ska alla produkterna visas.</p>
+      <div>
+        <h2>Products</h2>
+        <Grid templateColumns="repeat(5, 1fr)" gap={3
+        }>
+          {products.map((product, index) => (
+            <GridItem key={index}>
+              <Box width="300px" height="200px">
+                <Image
+                  src={product.image}
+                  alt={product.title}
+                  objectFit="cover"
+                  width="100%"
+                  height="100%"
+                />
+              </Box>
+            </GridItem>
+          ))}
+        </Grid>
+      </div>
     </Box>
   );
 }
