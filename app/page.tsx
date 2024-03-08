f451fbd5f1925bae1dbd8d20e0a3c61ed361bb80
Tomoyo Alvåg <tomoyo.tomoyo.m@gmail.com>
2024-03-08 11:13:25 +0100

    add products name and price

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { products } from "@/data";
-import { Box, Grid, GridItem, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  GridItem,
+  Image,
+  Link,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { LuHeart } from "react-icons/lu";
 
 export default function Home() {
   return (
@@ -9,22 +19,43 @@ export default function Home() {
       <p>Det här är startsidan. Här ska alla produkterna visas.</p>
       <div>
         <h2>Products</h2>
-        <Grid templateColumns="repeat(5, 1fr)" gap={3
-        }>
+        <SimpleGrid columns={4} gap={3}>
           {products.map((product, index) => (
             <GridItem key={index}>
-              <Box width="300px" height="200px">
+              <Flex flexDirection="column" width="300px" height="100%">
                 <Image
                   src={product.image}
                   alt={product.title}
                   objectFit="cover"
                   width="100%"
-                  height="100%"
+                  maxHeight="200px"
                 />
-              </Box>
+                <Box mt={2} flex="1">
+                  <Flex
+                    justifyContent="flex-start"
+                    flexDirection="column"
+                    height="100%"
+                  >
+                    <Text>{product.title}</Text>
+                    <Text>{product.price}kr</Text>
+                  </Flex>
+                </Box>
+                <Flex justifyContent="flex-end">
+                  <Link
+                    href="/"
+                    color="black"
+                    _hover={{ color: "blue.500" }}
+                    textDecor="none"
+                    mr={2}
+                  >
+                    <LuHeart />
+                  </Link>
+                  <HiOutlineShoppingBag />
+                </Flex>
+              </Flex>
             </GridItem>
           ))}
-        </Grid>
+        </SimpleGrid>
       </div>
     </Box>
   );
