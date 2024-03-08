0ae414a6b9fa9155860daf5af9455759796c4da9
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-03-08 12:27:26 +0100

    new footer image and layout

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Image } from "@chakra-ui/react";
+import { Flex, Image, Stack, Container } from "@chakra-ui/react";
 
 function Footer() {
   return (
@@ -8,20 +8,27 @@ function Footer() {
       bg="black"
       color="gray.500"
       justify="space-around"
-      
     >
-      <Box boxSize="100" objectFit="cover">
-        <Image src="klarna.png" alt="klarna" />
-      </Box>
-      <Box boxSize="100px">
-        <Image src="visa.png" alt="visa" />
-      </Box>
-      <Box boxSize="100px">
-        <Image src="americanEx.png" alt="american express" />
-      </Box>
-      <Box boxSize="100px">
-        <Image src="swish.png" alt="swish" />
-      </Box>
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        py={4}
+        spacing={4}
+        justify={"center"}
+        align={"center"}
+      >
+        <Stack direction={"row"} spacing={6}>
+          <Image src="klarna.png" alt="klarna" boxSize="100" />
+          <Image src="visa.png" alt="visa" boxSize="100px" />
+          <Image src="master.png" alt="mastercard" boxSize="100px" />
+          <Image
+            src="american.png"
+            alt="american express"
+            boxSize="100px"
+          />
+          <Image src="swish.png" alt="swish" boxSize="100px" />
+        </Stack>
+      </Container>
     </Flex>
   );
 }
