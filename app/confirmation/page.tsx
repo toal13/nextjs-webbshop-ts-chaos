6576b77947b81aea93780615b8a205a31c397d98
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-03-17 22:43:12 +0100

    add order details to confirmation

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -1,15 +1,45 @@
 "use client";
 
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Image,
+  Table,
+  Flex,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Tfoot,
+} from "@chakra-ui/react";
+import { useCart } from "../contexts/CartContext";
 
 export default function Confirmation() {
+  const { cart } = useCart();
+
+  const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    cart.forEach((item) => {
+      totalPrice += Number(item.price) * item.quantity;
+    });
+    return totalPrice;
+  };
+
   return (
     <Container maxW="container.lg" py={10}>
-      <Box textAlign="center">
+      <Box>
         <Heading as="h1" mb={6}>
           Order Confirmation
         </Heading>
-        <Text mb={6}>Order Number: Random number</Text>
+        <Text mb={6}>
+          Order Number:{" "}
+          <Text as="span" fontWeight="bold">
+            Random number
+          </Text>
+        </Text>
         <Text mb={6}>Thank you for choosing to shop with us.</Text>
         <Text mb={6}>
           {" "}
@@ -20,6 +50,37 @@ export default function Confirmation() {
           completely satisfied with your order. Welcome back. Nest Desgin
         </Text>
       </Box>
+      <Table variant="simple">
+        <Thead bg="gray.200">
+          <Tr>
+            <Th>Product</Th>
+            <Th>Quantity</Th>
+            <Th>Price</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {cart.map((item) => (
+            <Tr key={item.id}>
+              <Td>
+                <Flex alignItems="center">
+                  <Image src={item.image} alt={item.title} w="50px" h="50px" />
+                  <Text ml="2">{item.title}</Text>
+                </Flex>
+              </Td>
+              <Td>{item.quantity}</Td>
+              <Td>{item.price}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+        <Tfoot bg="gray.200">
+          <Tr>
+            <Td fontWeight="bold" colSpan={2}>
+              Total:
+            </Td>
+            <Td fontWeight="bold">{calculateTotalPrice()} kr</Td>
+          </Tr>
+        </Tfoot>
+      </Table>
     </Container>
   );
 }
