1d2748deb4ba0f892185d21c90e368434ca572ca
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-03-17 21:03:58 +0100

    add content to bekräftesesidan

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+"use client";
+
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 
 export default function Confirmation() {
-  return <div>Confirmation</div>;
+  return (
+    <Container maxW="container.lg" py={10}>
+      <Box textAlign="center">
+        <Heading as="h1" mb={6}>
+          Order Confirmation
+        </Heading>
+        <Text mb={6}>Order Number: Random number</Text>
+        <Text mb={6}>Thank you for choosing to shop with us.</Text>
+        <Text mb={6}>
+          {" "}
+          We understand that you are just dying to get your new design favorite
+          home, and we promise to do our best to ship your order as soon as we
+          can. Our ambition is to help you create a home to love and to which
+          you proudly open the door. We therefore hope that you will be
+          completely satisfied with your order. Welcome back. Nest Desgin
+        </Text>
+      </Box>
+    </Container>
+  );
 }
