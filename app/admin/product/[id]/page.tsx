f43b3ea2ccc24e35d01115fa88e5b3c3012027d1
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-03-20 16:57:53 +0100

    styling of edit page

--- app/admin/product/[id]/page.tsx
+++ app/admin/product/[id]/page.tsx
@@ -2,12 +2,15 @@
 
 import ProductForm from "@/app/components/ProductForm";
 import { products } from "@/data";
-import { Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { DiProlog } from "react-icons/di";
 
 type PageProps = { params: { id: string } };
 
 export default function AdminEditProductPage({ params }: PageProps) {
   const product = products.find((p) => p.id === params.id);
+  const [imagePreview, setImagePreview] = useState("");
 
   if (!product) {
     return (
@@ -20,8 +23,56 @@ export default function AdminEditProductPage({ params }: PageProps) {
 
   return (
     <div>
-      <Heading>The edit product page.</Heading>
-      <ProductForm product={product} />
+      <Heading textAlign="center" mt="2rem">
+        Edit Product Form
+      </Heading>
+      <Flex
+        flexDir={{ base: "column", md: "row" }}
+        rounded="lg"
+        justify="center"
+        alignItems="center"
+        width="80%"
+        m="2rem auto"
+        p="1rem"
+        gap={{ base: "1rem", md: "2rem" }}
+      >
+        <Box
+          bg="#FEF0D9"
+          width={{ base: "350px", md: "400px" }}
+          height={{ base: "300px", md: "480px" }}
+          rounded="lg"
+          overflow="hidden"
+          position="relative"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          {!imagePreview && (
+            <Flex gap='0.5rem'>
+              <Text
+                color="orange.400"
+                fontWeight="bold"
+                fontSize="1.5rem"
+                textAlign="center"
+                ml="1rem"
+              >
+                Preview Image
+              </Text>
+              <DiProlog fontSize="2.5rem" color="black" />
+            </Flex>
+          )}
+          {imagePreview && (
+            <Image
+              width="100%"
+              height="100%"
+              objectFit="cover"
+              src={imagePreview}
+              alt="Preview product image"
+            />
+          )}
+        </Box>
+        <ProductForm setImagePreview={setImagePreview} product={product} />
+      </Flex>
     </div>
   );
 }
