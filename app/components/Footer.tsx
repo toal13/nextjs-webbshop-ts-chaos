ba0f6d4714e73b29624b81b59f682d6ec772cbbc
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-03-21 12:22:27 +0100

    footer touch up

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,32 +1,50 @@
-import { Flex, Image, Square, Stack } from "@chakra-ui/react";
+import { Flex, Image, Square, Stack, Box, Container } from "@chakra-ui/react";
 
 function Footer() {
   return (
-    <Flex
-      as='footer'
-      py='5%'
-      bg='black'
-      color='gray.500'
-      justify='space-around'
-    >
-      <Stack direction={"row"} spacing={2}>
-        <Square size={{ base: "60px", md: "100px" }} data-cy='product-quantity'>
-          <Image src='klarna.png' alt='klarna' />
-        </Square>
-        <Square size={{ base: "60px", md: "100px" }} data-cy='product-quantity'>
-          <Image src='visa.png' alt='visa' />
-        </Square>
-        <Square size={{ base: "60px", md: "100px" }} data-cy='product-quantity'>
-          <Image src='master.png' alt='mastercard' />
-        </Square>
-        <Square size={{ base: "60px", md: "100px" }} data-cy='product-quantity'>
-          <Image src='american.png' alt='american express' />
-        </Square>
-        <Square size={{ base: "60px", md: "100px" }} data-cy='product-quantity'>
-          <Image src='swish.png' alt='swish' />
-        </Square>
-      </Stack>
-    </Flex>
+    <Box bg="black">
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        py={4}
+        spacing={4}
+        justify={"center"}
+        align={"center"}
+      >
+        <Stack direction={"row"} spacing={6}>
+          <Square
+            size={{ base: "60px", md: "100px" }}
+            data-cy="product-quantity"
+          >
+            <Image src="klarna.png" alt="klarna" />
+          </Square>
+          <Square
+            size={{ base: "60px", md: "100px" }}
+            data-cy="product-quantity"
+          >
+            <Image src="visa.png" alt="visa" />
+          </Square>
+          <Square
+            size={{ base: "60px", md: "100px" }}
+            data-cy="product-quantity"
+          >
+            <Image src="master.png" alt="mastercard" />
+          </Square>
+          <Square
+            size={{ base: "60px", md: "100px" }}
+            data-cy="product-quantity"
+          >
+            <Image src="american.png" alt="american express" />
+          </Square>
+          <Square
+            size={{ base: "60px", md: "100px" }}
+            data-cy="product-quantity"
+          >
+            <Image src="swish.png" alt="swish" />
+          </Square>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
 
