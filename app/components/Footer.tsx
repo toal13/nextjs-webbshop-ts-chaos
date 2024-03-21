eaa4093eaf5961e66c7f6431fb24a2835e14cfda
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-03-21 14:26:01 +0100

    Add more content to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,50 +1,47 @@
-import { Flex, Image, Square, Stack, Box, Container } from "@chakra-ui/react";
+import { Box, Container, Image, Square, Stack, Text } from "@chakra-ui/react";
+import { MiddleFooter } from "./MiddleFooter";
 
 function Footer() {
   return (
+    <>
+    <MiddleFooter />
     <Box bg="black">
       <Container
         as={Stack}
         maxW={"6xl"}
-        py={4}
+        pt={4}
         spacing={4}
         justify={"center"}
         align={"center"}
       >
         <Stack direction={"row"} spacing={6}>
-          <Square
-            size={{ base: "60px", md: "100px" }}
-            data-cy="product-quantity"
-          >
+          <Square size={{ base: "60px", md: "100px" }}>
             <Image src="klarna.png" alt="klarna" />
           </Square>
-          <Square
-            size={{ base: "60px", md: "100px" }}
-            data-cy="product-quantity"
-          >
+          <Square size={{ base: "60px", md: "100px" }}>
             <Image src="visa.png" alt="visa" />
           </Square>
-          <Square
-            size={{ base: "60px", md: "100px" }}
-            data-cy="product-quantity"
-          >
+          <Square size={{ base: "60px", md: "100px" }}>
             <Image src="master.png" alt="mastercard" />
           </Square>
-          <Square
-            size={{ base: "60px", md: "100px" }}
-            data-cy="product-quantity"
-          >
+          <Square size={{ base: "60px", md: "100px" }}>
             <Image src="american.png" alt="american express" />
           </Square>
-          <Square
-            size={{ base: "60px", md: "100px" }}
-            data-cy="product-quantity"
-          >
+          <Square size={{ base: "60px", md: "100px" }}>
             <Image src="swish.png" alt="swish" />
           </Square>
         </Stack>
       </Container>
+      <Box pb={6}>
+        <Text fontSize={"sm"} textAlign={"center"} color="whitesmoke">
+          Next Design By Group Chaos
+        </Text>
+        <Text fontSize={"sm"} textAlign={"center"} color="whitesmoke">
+          © 2024 All rights reserved
+        </Text>
+      </Box>
     </Box>
+    </>
   );
 }
 
