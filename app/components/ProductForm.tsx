925d826b89c7d292a4d737fa87d75d403a87d0b8
CatAvadani <catalinaavadani@gmail.com>
2024-03-22 11:24:38 +0100

    Autograding test

--- app/components/ProductForm.tsx
+++ app/components/ProductForm.tsx
@@ -74,47 +74,47 @@ export default function ProductForm(props: Props) {
             }
           };
           return (
-            <Form data-cy="product-form">
-              <Field name="title">
+            <Form data-cy='product-form'>
+              <Field name='title'>
                 {({ field, form }: any) => (
                   <FormControl
-                    mt="2%"
+                    mt='2%'
                     isInvalid={form.errors.title && form.touched.title}
                   >
                     <FormLabel>Title</FormLabel>
                     <Input
                       {...field}
-                      data-cy="product-title"
-                      autoComplete="title"
-                      focusBorderColor="brand.400"
+                      data-cy='product-title'
+                      autoComplete='title'
+                      focusBorderColor='brand.400'
                     />
-                    <FormErrorMessage data-cy="product-title-error">
+                    <FormErrorMessage data-cy='product-title-error'>
                       {form.errors.title}
                     </FormErrorMessage>
                   </FormControl>
                 )}
               </Field>
-              <Field name="id">
+              <Field name='id'>
                 {({ field, form }: any) => (
                   <FormControl
-                    mt="2%"
+                    mt='2%'
                     isInvalid={form.errors.id && form.touched.id}
                   >
                     <FormLabel>Product Id</FormLabel>
                     <Input
                       {...field}
-                      data-cy="product-id"
-                      autoComplete="id"
-                      focusBorderColor="brand.400"
+                      data-cy='product-id'
+                      autoComplete='id'
+                      focusBorderColor='brand.400'
                     />
                     <FormErrorMessage>{form.errors.id}</FormErrorMessage>
                   </FormControl>
                 )}
               </Field>
-              <Field name="description">
+              <Field name='description'>
                 {({ field, form }: any) => (
                   <FormControl
-                    mt="2%"
+                    mt='2%'
                     isInvalid={
                       form.errors.description && form.touched.description
                     }
@@ -122,61 +122,61 @@ export default function ProductForm(props: Props) {
                     <FormLabel>Description</FormLabel>
                     <Input
                       {...field}
-                      data-cy="product-description"
-                      autoComplete="description"
-                      focusBorderColor="brand.400"
+                      data-cy='product-description'
+                      autoComplete='description'
+                      focusBorderColor='brand.400'
                     />
-                    <FormErrorMessage data-cy="product-description-error">
+                    <FormErrorMessage data-cy='product-description-error'>
                       {form.errors.description}
                     </FormErrorMessage>
                   </FormControl>
                 )}
               </Field>
-              <Field name="image">
+              <Field name='image'>
                 {({ field, form }: any) => (
                   <FormControl
-                    mt="2%"
+                    mt='2%'
                     isInvalid={form.errors.image && form.touched.image}
                   >
                     <FormLabel>Image</FormLabel>
                     <Input
                       {...field}
-                      data-cy="product-image"
-                      autoComplete="image"
-                      focusBorderColor="brand.400"
+                      data-cy='product-image'
+                      autoComplete='image'
+                      focusBorderColor='brand.400'
                       onChange={handleImageChange}
                     />
-                    <FormErrorMessage data-cy="product-image-error">
+                    <FormErrorMessage data-cy='product-image-error'>
                       {form.errors.image}
                     </FormErrorMessage>
                   </FormControl>
                 )}
               </Field>
-              <Field name="price">
+              <Field name='price'>
                 {({ field, form }: any) => (
                   <FormControl
-                    mt="2%"
+                    mt='2%'
                     isInvalid={form.errors.price && form.touched.price}
                   >
                     <FormLabel>Price</FormLabel>
                     <Input
                       {...field}
-                      data-cy="product-price"
-                      autoComplete="price"
-                      focusBorderColor="brand.400"
+                      data-cy='product-price'
+                      autoComplete='price'
+                      focusBorderColor='brand.400'
                     />
-                    <FormErrorMessage data-cy="product-price-error">
+                    <FormErrorMessage data-cy='product-price-error'>
                       {form.errors.price}
                     </FormErrorMessage>
                   </FormControl>
                 )}
               </Field>
               <Button
-                mt="2rem"
-                type="submit"
-                bg="#E4A757"
+                mt='2rem'
+                type='submit'
+                bg='#E4A757'
                 _hover={{ bg: "#efdbc2" }}
-                variant="solid"
+                variant='solid'
                 isLoading={formikProps.isSubmitting}
                 onClick={() => formikProps.handleSubmit()}
               >
